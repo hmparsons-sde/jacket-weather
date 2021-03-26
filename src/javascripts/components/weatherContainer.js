fd59a675282d166544c98fe47733c482c25c09d7
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-03-25 20:04:49 -0500

    finishing touches

--- src/javascripts/components/weatherContainer.js
+++ src/javascripts/components/weatherContainer.js
@@ -1,11 +1,11 @@
 const searchForm = () => {
   document.querySelector('#search-box').innerHTML = `
-  <form id="get-weather" class="shadow p-3 rounded">
+  <form id="get-weather" class="shadow-lg p-4 rounded">
   <div class="form-group">
-    <h2 for="search" id="search-title">Get the Weather</h2>
-    <input type="text" class="form-control" id="location" aria-describedby="location" placeholder="zip code, city, etc.">
+    <h1 for="search" id="search-title" class="text-center mb-3 mt-3">Do you need a jacket?</h1>
+    <input type="text" class="form-control mt-4" id="location" aria-describedby="location" placeholder="Enter City Name">
   </div>
-  <button type="submit" id="search-weather" class="btn btn-outline-dark">Submit</button>
+  <button type="submit" id="search-weather" class="btn btn-outline-dark mt-2 ml-1 mb-2">Let's see!</button>
 </form>`;
 };
 
