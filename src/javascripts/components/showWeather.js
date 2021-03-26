2dc0987bc0f3941cdd593572852dac0a8900270c
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-03-25 19:32:50 -0500

    styling

--- src/javascripts/components/showWeather.js
+++ src/javascripts/components/showWeather.js
@@ -1,6 +1,6 @@
 const showWeather = (weatherObject) => {
   document.querySelector('#weather-cards').innerHTML += `
-  <div class="card" style="width: 10rem; height:20rem;">
+  <div class="mt-5 p-3 card text-center" style="width: 12rem; height:20rem;">
   <img src="http://openweathermap.org/img/wn/${weatherObject.weather[0].icon}@2x.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${weatherObject.name}</h5>
